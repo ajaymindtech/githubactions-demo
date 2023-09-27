45030ab1314ef7e846bcc12d1d0cf5860a94cce7
ajaymindtech <ajaymindtech2021@gmail.com>
2023-09-27 13:33:46 +0530

    add the nodejs applcation

---
+++ my-node-app/src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
